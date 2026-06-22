890e79eb21145903270e6d03ac2e1279936decca
Kuldeep <kunalrajput67718@gmail.com>
2026-06-22 23:15:47 +0530

    feat: inject mutation observer to strip intrusive bis_skin_checked attributes from DOM

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,6 +10,40 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning={true}>
       <head>
         <link rel="icon" type="image/png" href="/favicon.png" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                const removeAttr = (el) => {
+                  if (el.removeAttribute) {
+                    el.removeAttribute('bis_skin_checked');
+                  }
+                };
+                const observer = new MutationObserver((mutations) => {
+                  mutations.forEach((m) => {
+                    if (m.type === 'attributes' && m.attributeName === 'bis_skin_checked') {
+                      removeAttr(m.target);
+                    }
+                    if (m.addedNodes) {
+                      m.addedNodes.forEach((n) => {
+                        if (n.nodeType === 1) {
+                          removeAttr(n);
+                          n.querySelectorAll('[bis_skin_checked]').forEach(removeAttr);
+                        }
+                      });
+                    }
+                  });
+                });
+                observer.observe(document.documentElement, {
+                  childList: true,
+                  subtree: true,
+                  attributes: true,
+                  attributeFilter: ['bis_skin_checked']
+                });
+              })();
+            `,
+          }}
+        />
       </head>
       <body suppressHydrationWarning={true}>{children}</body>
     </html>
